d57655abc6c9d5439abfb623dd24ffea98e28556
Viacheslav Denysko <deniskosv@gmail.com>
2020-04-02 17:51:47 +0300

    fixed

--- lesson06 - hard/script/script.js
+++ lesson06 - hard/script/script.js
@@ -1,15 +1,25 @@
 'use strict';
 
-let num = 266219;
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-let num1 = num.toString().split('');
-let mult =1;
-for (let i=0; i<num1.length; i++){
-    mult *= num1[i];
+
+function Game(message = 'Угадай число от 1 до 100') {
+  let input = prompt(message);
+  let ourNum = Math.floor(Math.random() * 101);
+  console.log('input: ', input, 'ourNum: ', ourNum);
+  if (input === null) {
+    return alert('Игра окончена!');
+  } else if (!isNumber(input)) {
+    return Game("Введите число!");
+  } else if (input < ourNum) {
+    return Game("Загаданное число больше!");
+  } else if (input > ourNum) {
+    return Game("Загаданное число меньше!");
+  } else if (+ input === ourNum) {
+    return alert('Вы угадали число!');
+  }
 }
 
-console.log(mult);
-mult = mult**3;
-let output = mult.toString().split('');
-let alertWindow = output[0] + output[1];
-alert(alertWindow);
+Game();
--- lesson06/script/script.js
+++ lesson06/script/script.js
@@ -4,21 +4,22 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let ourNum = Math.floor(Math.random() * 101);
-function Game(ourNum, message = 'Угадай число от 1 до 100') {
+
+function Game(message = 'Угадай число от 1 до 100') {
   let input = prompt(message);
+  let ourNum = Math.floor(Math.random() * 101);
   console.log('input: ', input, 'ourNum: ', ourNum);
   if (input === null) {
     return alert('Игра окончена!');
   } else if (!isNumber(input)) {
-    return Game(ourNum, "Введите число!");
+    return Game("Введите число!");
   } else if (input < ourNum) {
-    return Game(ourNum, "Загаданное число больше!");
+    return Game("Загаданное число больше!");
   } else if (input > ourNum) {
-    return Game(ourNum, "Загаданное число меньше!");
+    return Game("Загаданное число меньше!");
   } else if (+ input === ourNum) {
     return alert('Вы угадали число!');
   }
 }
 
-Game(ourNum);
+Game();
