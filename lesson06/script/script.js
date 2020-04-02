aafa06354a3b487037da1862161552714f203924
Viacheslav Denysko <deniskosv@gmail.com>
2020-04-02 15:50:16 +0300

    completed common task of the lesson06

--- lesson06/script/script.js
+++ lesson06/script/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let ourNum = Math.floor(Math.random() * 101);
+function Game(ourNum, message = 'Угадай число от 1 до 100') {
+  let input = prompt(message);
+  console.log('input: ', input, 'ourNum: ', ourNum);
+  if (input === null) {
+    return alert('Игра окончена!');
+  } else if (!isNumber(input)) {
+    return Game(ourNum, "Введите число!");
+  } else if (input < ourNum) {
+    return Game(ourNum, "Загаданное число больше!");
+  } else if (input > ourNum) {
+    return Game(ourNum, "Загаданное число меньше!");
+  } else if (+ input === ourNum) {
+    return alert('Вы угадали число!');
+  }
+}
+
+Game(ourNum);
