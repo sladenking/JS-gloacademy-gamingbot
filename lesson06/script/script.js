52724ee47eec6fcc28b7730067c1da31277f56c9
Viacheslav Denysko <deniskosv@gmail.com>
2020-04-03 17:32:55 +0300

    fixed code

--- lesson06/script/script.js
+++ lesson06/script/script.js
@@ -5,21 +5,46 @@ let isNumber = function (n) {
 };
 
 
-function Game(message = 'Угадай число от 1 до 100') {
-  let input = prompt(message);
-  let ourNum = Math.floor(Math.random() * 101);
-  console.log('input: ', input, 'ourNum: ', ourNum);
-  if (input === null) {
-    return alert('Игра окончена!');
-  } else if (!isNumber(input)) {
-    return Game("Введите число!");
-  } else if (input < ourNum) {
-    return Game("Загаданное число больше!");
-  } else if (input > ourNum) {
-    return Game("Загаданное число меньше!");
-  } else if (+ input === ourNum) {
-    return alert('Вы угадали число!');
+const start = () => {
+  let ourNum = parseInt(Math.random() * 100);
+  function Game(ourNum, message = 'Угадай число от 1 до 100') {
+    let input = prompt(message);
+    console.log('input: ', input, 'ourNum: ', ourNum);
+    if (input === null) {
+      return alert('Игра окончена!');
+    } else if (!isNumber(input)) {
+      return Game(ourNum, "Введите число!");
+    } else if (input < ourNum) {
+      return Game(ourNum, "Загаданное число больше!");
+    } else if (input > ourNum) {
+      return Game(ourNum, "Загаданное число меньше!");
+    } else if (+ input === ourNum) {
+      return alert('Вы угадали число!');
+    }
   }
-}
+  
+  return Game(ourNum);
+  // console.dir(Game());
+};
 
-Game();
+start();
+
+
+// function Game(message = 'Угадай число от 1 до 100') {
+//   let input = prompt(message);
+//   let ourNum = Math.floor(Math.random() * 101);
+//   console.log('input: ', input, 'ourNum: ', ourNum);
+//   if (input === null) {
+//     return alert('Игра окончена!');
+//   } else if (!isNumber(input)) {
+//     return Game("Введите число!");
+//   } else if (input < ourNum) {
+//     return Game("Загаданное число больше!");
+//   } else if (input > ourNum) {
+//     return Game("Загаданное число меньше!");
+//   } else if (+ input === ourNum) {
+//     return alert('Вы угадали число!');
+//   }
+// }
+
+// Game();
